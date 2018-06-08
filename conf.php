75c86e3ee7a88a5c807d33b602f1bcb90087ce22
Jasmeetindybytes <39829700+Jasmeetindybytes@users.noreply.github.com>
2018-06-08 11:11:40 +0530

    updating conf.php

--- conf.php
+++ conf.php
@@ -1,4 +1,4 @@
 <?php
 
-	define('SHOPIFY_APP_API_KEY', '');
-	define('SHOPIFY_APP_SHARED_SECRET', '');
+	define('SHOPIFY_APP_API_KEY', 'df31641adc1f7657cd3ada87e768c17c');
+	define('SHOPIFY_APP_SHARED_SECRET', 'c846b936f38c1ee3fe6ec7fbbaef7348');
